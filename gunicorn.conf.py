583a3ba05e81db19539ace6ee9e7671b28c7854d
ourofinosp-ship-it <ourofinosp@gmail.com>
2025-09-20 15:38:27 -0300

    Add files via upload

---
+++ gunicorn.conf.py
@@ -0,0 +1,3 @@
+timeout = 300  # Aumenta o tempo limite para 300 segundos
+workers = 1    # Usa 1 worker para evitar sobrecarga no tier gratuito
+worker_tmp_dir = '/tmp'  # Usa /tmp para arquivos temporários do Gunicorn
